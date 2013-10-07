32a54a7ab33dd0e210906fdb423e43cd08d53ffa
Anna Kopp <koppanna@gmail.com>
2013-10-07 13:55:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShiftScheduler::Application.config.secret_token = '9e4ad91c683895de0436087b0fc43b200260d01740e88aafe93d45fcf144a2c2900b3ec161163745bedb4e729f73af618c4151b3a0eccf13ae89ebeffa8b1ee4'
