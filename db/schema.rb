efcb8a992075eaee40d66a7d9f0381299203c5c5
Anna Kopp <koppanna@gmail.com>
2013-10-17 14:40:30 -0400

    Removes traces of notif

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131011172538) do
+ActiveRecord::Schema.define(:version => 20131017183937) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
