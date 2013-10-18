d070bb4a0e22394843b05614cbb602ccfe3c4e05
Anna Kopp <koppanna@gmail.com>
2013-10-18 16:52:30 -0400

    Secures secret token in figaro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ShiftScheduler::Application.config.secret_token = '9e4ad91c683895de0436087b0fc43b200260d01740e88aafe93d45fcf144a2c2900b3ec161163745bedb4e729f73af618c4151b3a0eccf13ae89ebeffa8b1ee4'
+ShiftScheduler::Application.config.secret_token = ENV['SECRET_TOKEN']
